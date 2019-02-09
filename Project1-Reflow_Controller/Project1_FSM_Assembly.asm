b5c9e2ad4f1b337b33ca8314a73812c59f2a7a65
danielyzli <danielyzli222@users.noreply.github.com>
2019-02-09 12:44:22 -0800

    New README, some changes to FSM state 2

--- Project1-Reflow_Controller/Project1_FSM_Assembly.asm
+++ Project1-Reflow_Controller/Project1_FSM_Assembly.asm
@@ -12,7 +12,7 @@ START_BUTTON equ P4.5													; arbitrary pin for start button
 forever:
 
 	; non-blocking state machine for KEY1 starts here
-		mov a, State_Counter
+	mov a, STATE_COUNTER
 	wait:
 		cjne a, #WAIT, rampToSoak
 			; At this point we are in wait state state
@@ -23,38 +23,43 @@ forever:
 				; At this point, we have detected a valid press of the start button
 				cjne SET_MODE, #0, doneWaitState						; if we are setting something, keep waiting
 					; The following code executes when START_BUTTON has been pressed and we are not setting anything
-					mov State_Timer, #60								; Start a counter for 60s for the safety check
+					mov STATE_TIMER, #60								; Start a counter for 60s for the safety check
 					mov Current_Target_Temp+0, Soak_Temp+0				; Set the target temperature as Soak_Temp
 					mov Current_Target_Temp+1, Soak_Temp+1
 					mov Power, #100										; Ramp to next temperature as fast as possible
-					mov State_Counter, #RAMP_TO_SOAK 					; Go to next state
+					mov STATE_COUNTER, #RAMP_TO_SOAK 					; Go to next state
 					lcall stateChangeBeep 								; Little beep of the speaker to indicate state change
 			doneWaitState:
 				ljmp fsm1Done
 	rampToSoak:
 		cjne a, #RAMP_TO_SOAK, soak
 			; At this point we are in rampToSoak state
-			cjne State_Timer, #0, decCounter							; if our safety timer isn't zero we continue to count down
-				mov x, CURRENT_ACTUAL_TEMP
+			mov x+0, Current_Actual_Temp+0
+			mov x+1, Current_Actual_Temp+1
+			mov x+2, #0
+			mov x+3, #0
+			mov y, #Current_Target_Temp
+			lcall x_lt_y
+			jb mb, notAtSoak										; if Current_Actual_Temp<Current_Target_Temp don't move to SOAK
+				mov State_Timer, #SOAK_TIME							; set timer to length of soak period
+				mov STATE_COUNTER, #SOAK							; set state to SOAK
+				ljmp fsm1Done										; finish with current state and move on to forever to begin SOAK
+			notAtSoakTemp:
+			cjne STATE_TIMER, #0, decCounter							; if our safety timer isn't zero we continue to count down
+				mov x, #Current_Actual_Temp
 				mov y, #50
-				x_lteq_y
-				jb mb, errorTemp										; if CURRENT_ACTUAL_TEMP<=50 and our safety timer was zero
-					mov a, SOAK_TIME
-					mov State_Timer, a
-					mov a, SOAK
-					mov State_Counter, a
-					jmp forever
+				lcall x_lteq_y
+				jb mb, errorTemp										; if Current_Actual_Temp<=50 and our safety timer was zero
 				errorTemp:												; shut down and return to wait state after error
-					mov a, #0
-					mov Power, a
-					mov a, WAIT
-					mov State_Counter, a
+					mov Power, #0
+					mov STATE_COUNTER, #WAIT
 					lcall errorBeep
-					jmp forever
-		sjmp fsm1Done
+					ljmp fsm1Done
 	soak:
 		cjne a, #SOAK, rampToReflow
 			; At this point we are in soak state
+			cjne State_Timer, #0, decCounter							; if our safety timer isn't zero we continue to count down
+
 		sjmp fsm1Done
 	rampToReflow:
 		cjne a, #RAMP_TO_REFLOW, reflow
@@ -90,26 +95,26 @@ jmp forever
 			mov a, 100
 			mov Power, a										; Ramp to next temperature as fast as possible
 			mov a, RAMP_TO_SOAK
-			mov State_Counter, a 								; Go to next state
+			mov STATE_COUNTER, a 								; Go to next state
 			lcall stateChangeBeep 								; Little beep of the speaker to indicate state change
 			jmp forever
 ;1:
 	cjne State_Timer, #0, decCounter
-		;jb x_lteq_y(CURRENT_ACTUAL_TEMP, #50), errorTemp
-		mov x, CURRENT_ACTUAL_TEMP
+		;jb x_lteq_y(Current_Actual_Temp, #50), errorTemp
+		mov x, Current_Actual_Temp
 		mov y, #50
 		x_lteq_y
 		jb mb, errorTemp
 			mov a, SOAK_TIME
 			mov State_Timer, a
 			mov a, SOAK
-			mov State_Counter, a
+			mov STATE_COUNTER, a
 			jmp forever
 		errorTemp:
 			mov a, #0
 			mov Power, a
 			mov a, WAIT
-			mov State_Counter, a
+			mov STATE_COUNTER, a
 			lcall errorBeep
 			jmp forever
 	decCounter:
--- Project1-Reflow_Controller/README.md
+++ Project1-Reflow_Controller/README.md
@@ -1,1 +1,18 @@
-This is a repository for Project 1 - Reflow Oven Controller for ELEC 291/292.
+###### This is a repository for Project 1 - Reflow Oven Controller for ELEC 291/292.
+
+# Reminders when coding:
+*Format:*
+	- Symbolic Constants, Buttons/Pins: CAPS_CAPS_CAPS
+	- Global Variables (other than x, y, bcd for math32): Uppercase_Uppercase
+	- Labels (for jumping): lowercaseUppercase
+	- Function names: UpperCaseUppercase
+	"ISR" and "LED" can be capitalized regardless of guidelines
+**Please use hard tabs (tab characters) instead of spaces, makes cleaning up formatting much easier**
+
+*Code:*
+	- When using math32.inc, and using registers "x" and "y", make sure to move as follows:
+    mov x+0, variable+0		; Move low bits of "variable" into "x"
+    mov x+1, variable+1		; Move higher bits of "variable" into "x"
+    mov x+2, #0				; Clear
+    mov x+3, #0				; Clear (these help make sure no residue)
+	
